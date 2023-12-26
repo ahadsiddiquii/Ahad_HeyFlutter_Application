06cfce0c2311b472d055cde29da6ed6db27ca3a6
Ahad ur Rehman Siddiqui <ahad12siddiqui34@gmail.com>
2023-12-26 15:23:25 +0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+import 'src/ui/configs/app_config/app_config.dart';
+import 'src/ui/configs/app_config/environment_flavours_enum.dart';
+import 'src/ui/configs/local_storage_sqflite/local_storage_sqflite.dart';
+
+Future<void> main() async {
+  //Settings
+  WidgetsFlutterBinding.ensureInitialized();
+  Paint.enableDithering = true;
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  //Local Database Setup
+  await LocalStorageSqfLite.initDatabase();
+
+  //Appconfig initialization
+  AppConfig.init(
+    environment: EnvironmentFlavours.staging,
+    appVersionToShow: '0.0.0',
+  );
+
   runApp(const MyApp());
 }
 
---
+++ lib/src/ui/generic/constants/theme_colors.dart
@@ -0,0 +1,34 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  ThemeColors._();
+  //Application Theme Colors
+  static const Color kThemeColor = Color(0xFF78BBFF);
+
+  //Screen Required Colors
+  static const Color kAppBarColor = kThemeColor;
+  static const Color kScaffoldBackgroundColor = Colors.white;
+  static const Color kButtonColor = kThemeColor;
+  static const Color kButtonSecondaryColor = Color(0xFFF1F2F6);
+  static const Color kNavBarBackgroundBlueColor = Color(0xFF325F8D);
+  static const Color kButtonDarkBlueColor = Color(0xFF325F8D);
+  static const Color kButtonTextColor = Colors.white;
+  static const Color kButtonTextSecondaryColor = Color(0xFFC0C0C0);
+  static const Color kErrorRedColor = Color(0xFFD44B3B);
+
+  //Font Colors
+  static const Color kFontPrimaryBlueColor = Color(0xFF4C739B);
+  static const Color kFontSecondaryBlueColor = Color(0xFF325F8D);
+  static const Color kFontSecondaryGreyColor = Color(0xFF727272);
+
+  static const Color kFontHintColor = Color(0xFFD1D1D1);
+  static const Color kFontSearchHintColor = Color(0xFFD5D5D5);
+
+  //Product Tile Display
+  static const Color kIconButtonGreyColor = Color(0xFFCBCBCB);
+  static const Color kIconButtonLightGreyColor = Color(0xFFE6E6E6);
+
+  static const Color kMyAccountDividerColor =
+      Color.fromARGB(255, 238, 238, 240);
+}
