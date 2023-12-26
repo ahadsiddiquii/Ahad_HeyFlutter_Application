e1d6a169524bf7fb4577e1d43ef962e56c124bd5
Ahad ur Rehman Siddiqui <ahad12siddiqui34@gmail.com>
2023-12-26 16:22:32 +0500

    Feature: Onboarding setup flow
    1. welcome screen

---
+++ lib/src/ui/generic/assets/image_assets.dart
@@ -0,0 +1,9 @@
+class ImageAssets {
+  ImageAssets._();
+  //Default Image Path
+  static const String _imagePath = 'assets/images/';
+
+  //Onboarding Assets
+  static const DUMMY_BACKGROUND_IMAGE =
+      '${_imagePath}dummy_background_image.png';
+}
